d22b9e2baf83b411a6a58b59099a87716f5b3de6
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-09-15 00:55:06 +0100

    embillir l'application

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -17,8 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'flutter first app',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 13, 124, 57)),
+          colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFF00FF00)),
         ),
         home: MyHomePage(),
       ),
@@ -28,9 +28,11 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  int intValue = Random().nextInt(100) + 50;
 
   void getNext() {
     current = WordPair.random();
+    intValue = Random().nextInt(25) + 5;
     notifyListeners(); // toutes personne surveillant MyAppState est au courant de changement
   }
 }
@@ -39,12 +41,14 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var mot = appState.current;
+    var val = appState.intValue;
     return Scaffold(
       body: Column(
         children: [
           Text('Salut Ouadjih you Can do it ! '),
-          Text(appState.current.asPascalCase),
+          BigCard(mot: mot),
+          Text("new num : $val"),
           ElevatedButton(
             onPressed: () {
               appState.getNext();
@@ -52,6 +56,31 @@ class MyHomePage extends StatelessWidget {
             child: Text('Next'),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.mot,
+  });
+
+  final WordPair mot;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(mot.asUpperCase, style: style),
       ),
     );
   }
